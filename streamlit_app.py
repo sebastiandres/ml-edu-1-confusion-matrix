11476be7815a38e7a02cf9b2d16209684beab2fe
sebastiandres <sebastiandres@gmail.com>
2022-09-09 23:53:41 -0400

    renamed path due to multipaging

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,14 @@ st.set_page_config(page_title="Confusion Matrix",
                     layout="centered")
 
 # Streamit book properties
-stb.set_chapter_config(path="pages",
-                    button="bottom",
-                    button_next="Next",
-                    button_previous="Previous",
-                    button_refresh="Reload",
-                    on_load_header=magic.matrix_effect,
-                    toc=False)
+stb.set_chapter_config(
+                        path="docs",
+                        button="bottom",
+                        button_next="Next",
+                        button_previous="Previous",
+                        button_refresh="Reload",
+                        on_load_header=magic.matrix_effect,
+                        toc=False)
 
 # Using a specific theme from The Matrix: .streamlit/config.toml 
 # See https://blog.streamlit.io/introducing-theming/
